58dbdc6d2dafb07aeb1875869b06ace716044dfe
Lucas Felipe <lucasfelipemk@gmail.com>
2022-11-03 10:15:43 -0300

    aula 04 done

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ function createGame(player1, hour,player2){
     `
 }
 
+let delay = -0.3;
+
 function createCard(date,day,game){
+    delay = delay + 0.3;
     return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
         <h2>${date} <span>${day}</span></h2>
         <ul>
            ${game}
@@ -19,22 +22,12 @@ function createCard(date,day,game){
     `
 }
 
-document.querySelector('#app').innerHTML = `
-    <header>
-    <img src="./assets/logo.svg" alt="Logo na NLW">
-    </header>
-
-    <main id="cards">
-    ${createCard('21/11', 'quinta',
-    createGame('switzerland','07:00','cameroon') +
+document.querySelector('#cards').innerHTML =
+    createCard('21/11', 'quinta',createGame('switzerland','07:00','cameroon') +
     createGame('portugal','13:00','argentina') +
-    createGame('brazil','16:00','serbia')
+    createGame('brazil','16:00','serbia')) +
 
-    )}
-    ${createCard('28/11','segunda',
-        createGame('japan','10:00','cameroon') +
+    createCard('28/11','segunda',createGame('japan','10:00','cameroon') +
         createGame('brazil','13:00','switzerland') +
         createGame('portugal','16:00','colombia')
-    )}
-    </main>
-`
+    )
